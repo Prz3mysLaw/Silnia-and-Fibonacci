7e912e32bf837b575101cdd0fbcef6a6a6221b0b
bboyKamel <nacoon@tlen.pl>
2019-02-13 13:28:42 +0100

    Silnia factory iterational test

--- src/test/java/SilniaFactoryTest.java
+++ src/test/java/SilniaFactoryTest.java
@@ -24,6 +24,26 @@ public class SilniaFactoryTest {
 
     }
 
+    @Test
+    public void shouldReturn120When5thProductWasRequestedIterationalUsingFactory(){
+        //given
+        final int expectedProduct = 120;
+        final int requestProduct = 5;
+        final SilniaType type = SilniaType.ITERATIONAL;
+
+        Silnia silniaUnderTest = SilniaFactory.produce(type);
+
+        //when
+        int result = silniaUnderTest.generate(requestProduct);
+
+        //then
+
+        Assert.assertEquals(expectedProduct, result);
+        Assert.assertTrue(silniaUnderTest instanceof Silnia);
+        Assert.assertFalse(silniaUnderTest instanceof SilniaRecursive);
+        Assert.assertTrue(silniaUnderTest instanceof SilniaIterational);
+    }
+
 
 
 }
